e68a43bb46b1879fc5ca6556bd2cc6252e0e3aaf
Mattias Selin <mattias.selin@eyevinn.se>
2020-04-27 14:03:56 +0200

    Set targetlatency from manifest/livedelay

--- library/core/src/main/java/com/google/android/exoplayer2/playback/MattesExperimentalPlaybackRateController.java
+++ library/core/src/main/java/com/google/android/exoplayer2/playback/MattesExperimentalPlaybackRateController.java
@@ -5,23 +5,19 @@ import com.google.android.exoplayer2.C;
 public final class MattesExperimentalPlaybackRateController implements PlaybackRateController {
     public static final float DEFAULT_CATCHUP_PLAYBACK_RATE = 1.3f;
     public static final long DEFAULT_MIN_DRIFT_MS = 500;
-    public static final long DEFAULT_TARGET_LATENCY_MS = 3000L;
 
     public static final class Builder {
         private float catchupPlaybackRate;
         private long maxDriftMs;
-        private long targetLatencyMs;
 
-        public Builder(float catchupPlaybackRate, long maxDriftMs, long targetLatencyMs) {
+        public Builder(float catchupPlaybackRate, long maxDriftMs) {
             this.catchupPlaybackRate = catchupPlaybackRate;
             this.maxDriftMs = maxDriftMs;
-            this.targetLatencyMs = targetLatencyMs;
         }
 
         public Builder() {
             this(DEFAULT_CATCHUP_PLAYBACK_RATE,
-                 DEFAULT_MIN_DRIFT_MS,
-                 DEFAULT_TARGET_LATENCY_MS);
+                 DEFAULT_MIN_DRIFT_MS);
         }
 
         public Builder setCatchupPlaybackRate(float catchupPlaybackRate) {
@@ -34,32 +30,24 @@ public final class MattesExperimentalPlaybackRateController implements PlaybackR
             return this;
         }
 
-        public Builder setTargetLatencyMs(long targetLatencyMs) {
-            this.targetLatencyMs = targetLatencyMs;
-            return this;
-        }
-
         public PlaybackRateController build() {
             return new MattesExperimentalPlaybackRateController(catchupPlaybackRate,
-                    maxDriftMs,
-                                                     targetLatencyMs);
+                    maxDriftMs);
         }
     }
 
     private final float catchupPlaybackRate;
     private final long maxDriftMs;
-    private final long targetLatencyMs;
 
-    private MattesExperimentalPlaybackRateController(float catchupPlaybackRate, long maxDriftMs, long targetLatencyMs) {
+    private MattesExperimentalPlaybackRateController(float catchupPlaybackRate, long maxDriftMs) {
         this.catchupPlaybackRate = catchupPlaybackRate;
         this.maxDriftMs = maxDriftMs;
-        this.targetLatencyMs = targetLatencyMs;
     }
 
     @Override
     public void onPositionsUpdated(SpeedHandler speedHandler, long positionUs, long liveTimeUs) {
         long latencyMs = C.usToMs(liveTimeUs - positionUs);
-        float latencyErrorMs = latencyMs-targetLatencyMs;
+        float latencyErrorMs = latencyMs;
 
         float severity = latencyErrorMs/maxDriftMs;
         severity = (severity < -1f) ? -1f : (severity > 1f ? 1f : severity);
